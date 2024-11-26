6cad98b8e103d6e3c0d25aedad3a6cf27fedad89
samirahekmati <samirahekmati@gmail.com>
2024-11-26 21:54:15 +0000

    fixed-calculateMedian

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,9 +3,31 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (list.length % 2 !== 0) {
+    const middleIndex = Math.floor(list.length / 2);
+    const median = list[middleIndex];
+    return median;
+  } else {
+    console.log("list.length", list.length);
+
+    const afterMiddleIndex = Math.floor(list.length / 2);
+    console.log("afterMiddleIndex", afterMiddleIndex);
+    const secondMedian = list[afterMiddleIndex];
+    console.log("secondMedian", secondMedian);
+
+    console.log("list.length", list.length);
+
+    const beforeMiddleIndex = Math.floor(list.length / 2) - 1;
+    console.log("beforeMiddleIndex", beforeMiddleIndex);
+    const firstMedian = list[beforeMiddleIndex];
+    console.log("firstMedian", firstMedian);
+    return average(secondMedian, firstMedian);
+  }
 }
 
+function average(num1, num2) {
+  return (num1 + num2) / 2;
+}
+
+//console.log(calculateMedian([12,42,73,94]))
 module.exports = calculateMedian;
