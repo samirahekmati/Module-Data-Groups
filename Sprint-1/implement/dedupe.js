3403f53558f22fd5e26a19d4e30402919b132d87
samirahekmati <samirahekmati@gmail.com>
2024-11-27 16:46:11 +0000

    testing dedupe function with jest

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    currentItem = arr[i];
+    if (!newArr.includes(currentItem)) {
+      newArr.push(currentItem);
+    }
+  }
+  return newArr;
+}
+
+module.exports = dedupe;
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,7 +16,7 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+//test.todo("given an empty array, it returns an empty array");
 
 // Given an array with no duplicates
 // When passed to the dedupe function
@@ -25,3 +25,18 @@ test.todo("given an empty array, it returns an empty array");
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+describe("dedupe", () => {
+  test("removes duplicate values of number from array", () => {
+    expect(dedupe([1, 2, 3, 3])).toEqual([1, 2, 3]);
+  });
+  test("return an empty array for an emtpy array", () => {
+    expect(dedupe([])).toEqual([]);
+  });
+  test("given an array with no duplicate returns copy o original array", () => {
+    expect(dedupe([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+  test("returns array of string without duplicates", () => {
+    expect(dedupe(["a", "a", "a", "b", "b", "c"])).toEqual(["a", "b", "c"]);
+  });
+});
