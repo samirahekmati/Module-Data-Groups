df913dc4091bd84c469f87a179ea2f1f98fd90fd
samirahekmati <samirahekmati@gmail.com>
2024-11-29 08:39:36 +0000

    tested sum function with jest

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,14 @@
 function sum(elements) {
+  let numericValues = elements.filter(function (item) {
+    return typeof item === "number";
+  });
+  let sum = 0;
+  for (let i = 0; i < numericValues.length; i++) {
+    sum = sum + numericValues[i];
+  }
+  return sum;
 }
+
+console.log("sum of elements =", sum(["hello"]));
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,7 +13,6 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
 
 // Given an array with just one number
 // When passed to the sum function
@@ -34,3 +33,23 @@ test.todo("given an empty array, returns 0")
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+describe("sum", () => {
+    test("Given an empty array,it should return 0", () =>{
+        expect(sum([])).toEqual(0)
+    });
+    test("Given an array with just one number,it should return that number", () =>{
+        expect(sum([20])).toEqual(20)
+    });
+    test("Given an array containing negative numbers,it should still return the correct total sum", () =>{
+        expect(sum([-6,-1,3])).toEqual(-4)
+    });
+    test("Given an array with decimal/float numbers,it should return the correct total sum", () =>{
+        expect(sum([3.1, 2.1, 5])).toEqual(10.2)
+    });
+    test("Given an array containing non-number values,it should ignore the non-numerical values and return the sum of the numerical elements", () =>{
+        expect(sum([1, 2, 3, "hello"])).toEqual(6)
+    });
+    test("Given an array with only non-number values,Then it should return the least surprising value given how it behaves for all other inputs", () =>{
+        expect(sum(['Hello'])).toEqual(0)
+    });
+})
