c0bd03ddddb301c89ca19775fb0082b04b0fed25
samirahekmati <samirahekmati@gmail.com>
2024-12-10 16:33:10 +0000

    tally.js implemented `tally` function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(items) {
+    if(!Array.isArray(items)){
+        throw new Error("Input must be an array")
+    }
+   
+    return items.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1; // Increment count or initialize to 1
+        return acc;
+      }, {}); // Initialize accumulator as an empty object
+
+}
+
+console.log("items", tally(['a', 'a', 'a', 'b']))
 
 module.exports = tally;
