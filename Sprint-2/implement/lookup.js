ea2c32d7f49a7d20c73fd62f2fcbbce34f59a0fa
samirahekmati <samirahekmati@gmail.com>
2024-12-10 16:08:51 +0000

    lookup.js implemented `createLookUp` function

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,18 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  return countryCurrencyPairs.reduce((lookup, pair) => {
+    const [country, currency] = pair; // Destructure the array into country and currency
+    lookup[country] = currency; // Assign the currency to the country code in the object
+    return lookup; // Return the updated lookup object
+  }, {}); // Initialize the accumulator as an empty object
 }
 
+console.log(createLookup([['US', 'USD'], ['CA', 'CAD']]))
+
+//test cases with assertion
+
+//test 1
+console.assert(JSON.stringify(createLookup([['US', 'USD'], ['CA', 'CAD']])) === JSON.stringify({
+  'US': 'USD',
+  'CA': 'CAD'
+}),"test 1 failed")
 module.exports = createLookup;
