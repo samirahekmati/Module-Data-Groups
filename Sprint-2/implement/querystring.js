d1f7dbfa579e06336a87dec25dca87374fde7033
samirahekmati <samirahekmati@gmail.com>
2024-12-14 13:44:24 +0000

    `parseQueryString` function completed

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -4,13 +4,28 @@ function parseQueryString(queryString) {
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
+  console.log("key value pairs after spliting &:", keyValuePairs)
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    const separatorIndex = pair.indexOf("=");
+    let key, value;
+
+    if (separatorIndex !== -1) {
+      key = decodeURIComponent(pair.slice(0, separatorIndex));
+      value = decodeURIComponent(pair.slice(separatorIndex + 1));
+    } else {
+      // Handle keys without values (e.g., "key&key2=value2")
+      key = decodeURIComponent(pair);
+      value = "";
+    }
+
     queryParams[key] = value;
   }
+  
 
   return queryParams;
 }
+
+console.log(parseQueryString("equation=x=y+1"))
 
 module.exports = parseQueryString;
